9339ff5cedf1d8866295af8e921aeb7dfe7e5093
BARKI MUSTAPHA <start.export@hotmail.com>
2025-09-09 14:19:05 -0300

    Initial commit of the web application

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{message}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/api/hello', (req, res) => {
+  res.send({ message: 'Hello from the server!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
